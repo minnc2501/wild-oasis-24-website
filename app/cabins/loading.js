9eb59bd4dd10a43e6293267ba4c56d65717f025a
minnc2501 <minz2501z@gmail.com>
2025-02-05 18:49:16 +0700

    fix loading cabin

--- app/cabins/loading.js
+++ app/cabins/loading.js
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="grid items-center justify-center">
       <Spinner />
-      <span className="text-xl text-primary-200">Loading cabin data...</span>
+      <span className="text-xl text-primary-200">Loading cabin...</span>
     </div>
   );
 }
